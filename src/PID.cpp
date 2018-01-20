7da2318ebb62da5bca9351e981be0488bead0075
janreerink <janreerink@hotmail.com>
2018-01-20 14:51:56 +0100

    Update PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,140 +1,32 @@
-#include <uWS/uWS.h>
-#include <iostream>
-#include "json.hpp"
 #include "PID.h"
-#include <math.h>
 
-// for convenience
-using json = nlohmann::json;
+using namespace std;
 
-// For converting back and forth between radians and degrees.
-constexpr double pi() { return M_PI; }
-double deg2rad(double x) { return x * pi() / 180; }
-double rad2deg(double x) { return x * 180 / pi(); }
+/*
+* TODO: Complete the PID class.
+*/
 
-// Checks if the SocketIO event has JSON data.
-// If there is data the JSON object in string format will be returned,
-// else the empty string "" will be returned.
-std::string hasData(std::string s) {
-  auto found_null = s.find("null");
-  auto b1 = s.find_first_of("[");
-  auto b2 = s.find_last_of("]");
-  if (found_null != std::string::npos) {
-    return "";
-  }
-  else if (b1 != std::string::npos && b2 != std::string::npos) {
-    return s.substr(b1, b2 - b1 + 1);
-  }
-  return "";
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+
+  p_error = 0;
+  i_error = 0;
+  d_error = 0;
 }
 
-int main()
-{
-  uWS::Hub h;
-  // TODO: Initialize the pid variable.
-  PID pid; //controller for steering
-
-  // set initial values for coefficients and their errors as in class:
-  // if only controlling steer angle use  0.075, 0, -3 and throttle 0.3
-  double Kp_i = -0.12;  //proportional term is negative (if right, steer left) --0.1
-  double Ki_i = -0.0; // integrative term; should only be necessary in case of systematic bias
-  double Kd_i = -3.9; // negative differential term; corrects oversteering ---3
-  pid.Init(Kp_i, Ki_i, Kd_i);
-
-  PID pidt; //controller for speed
-
-  double Kp_it = -0.3;  //proportional term
-  double Ki_it = -0.0; // integrative term; should only be necessary in case of systematic bias
-  double Kd_it = -0.1; // negative differential term
-
-
-  pidt.Init(Kp_it, Ki_it, Kd_it);
-
-
-
-
-
-
-  h.onMessage([&pid, &pidt](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
-    // "42" at the start of the message means there's a websocket message event.
-    // The 4 signifies a websocket message
-    // The 2 signifies a websocket event
-    if (length && length > 2 && data[0] == '4' && data[1] == '2')
-    {
-      auto s = hasData(std::string(data).substr(0, length));
-      if (s != "") {
-        auto j = json::parse(s);
-        std::string event = j[0].get<std::string>();
-        if (event == "telemetry") {
-          // j[1] is the data JSON object
-          double cte = std::stod(j[1]["cte"].get<std::string>());
-          double speed = std::stod(j[1]["speed"].get<std::string>());
-          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          double steer_value;
-          /*
-          * TODO: Calcuate steering value here, remember the steering value is
-          * [-1, 1].
-          * NOTE: Feel free to play around with the throttle and speed. Maybe use
-          * another PID controller to control the speed!
-          */
-          pid.UpdateError(cte); //update error given CTE
-          pidt.UpdateError(fabs(cte)); //update throttle given absolute CTE
-
-          steer_value = pid.TotalError(); //get new steer value
-          double throttle;
-          throttle = 0.8 + pidt.TotalError(); // get new throttle as default throttle minus correction in case of increasing CTE
-          // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
-
-          json msgJson;
-          msgJson["steering_angle"] = steer_value;
-          //msgJson["throttle"] = 0.3;
-          msgJson["throttle"] = throttle;
-          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-        }
-      } else {
-        // Manual driving
-        std::string msg = "42[\"manual\",{}]";
-        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-      }
-    }
-  });
-
-  // We don't need this since we're not using HTTP but if it's removed the program
-  // doesn't compile :-(
-  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
-    const std::string s = "<h1>Hello world!</h1>";
-    if (req.getUrl().valueLength == 1)
-    {
-      res->end(s.data(), s.length());
-    }
-    else
-    {
-      // i guess this should be done more gracefully?
-      res->end(nullptr, 0);
-    }
-  });
-
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-    std::cout << "Connected!!!" << std::endl;
-  });
-
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
-    ws.close();
-    std::cout << "Disconnected" << std::endl;
-  });
-
-  int port = 4567;
-  if (h.listen(port))
-  {
-    std::cout << "Listening to port " << port << std::endl;
-  }
-  else
-  {
-    std::cerr << "Failed to listen to port" << std::endl;
-    return -1;
-  }
-  h.run();
+void PID::UpdateError(double cte) {
+  d_error = cte - p_error; // differential term, previous error still stored in p-term
+  p_error = cte; // proportioanl term
+  i_error += cte; // integral term
 }
+
+double PID::TotalError() {
+  return Kp * p_error + Ki * i_error + Kd * d_error;
+}
+
